908ec4f9a8c517cf85821d2dccd3e1767d5e1923
Sathish Kumar <sathish@jupitersupplystation.com>
2020-06-14 05:28:51 +0530

    Add time complexity as comments

--- Array.swift
+++ Array.swift
@@ -10,6 +10,7 @@ class Array {
     }
   }
 
+  // Insertion is O(n) since it shifts all the values to right.
   func insert(_ index: Int, _ val: String) {
     var temp = arrayContent[index];
     arrayContent[index] = val
@@ -22,16 +23,19 @@ class Array {
     count += 1
   }
 
+  // Append or push is O(1)
   func append(val: String) {
     arrayContent[count] = val
     count += 1
   }
 
+  // O(1) operation
   func pop() {
     count -= 1
     arrayContent[count] = nil
   }
 
+  // Again O(n) operation, since unshifts all the values to the left. 
   func delete(_ index: Int) {
     for i in index...count-2 {
       arrayContent[i] = arrayContent[i+1]
